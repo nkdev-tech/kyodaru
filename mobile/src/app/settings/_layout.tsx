55cf6e6e3a037b2a8623dcf2eb60ae19889b85b5
Natsuki Katayama <xiaxipianshan7@gmail.com>
2026-06-27 20:12:45 +0900

    refactor(mobile): ネイティブヘッダーを使わないため不要なtitleオプションを削除する
    Stack の headerShown を false にしているため、各 Screen の
    title オプションは表示に使われず死んだ設定だった。

--- mobile/src/app/settings/_layout.tsx
+++ mobile/src/app/settings/_layout.tsx
@@ -3,8 +3,8 @@ import { Stack } from 'expo-router';
 export default function SettingsLayout() {
   return (
     <Stack screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="index" options={{ title: '設定' }} />
-      <Stack.Screen name="account" options={{ title: 'アカウント設定' }} />
+      <Stack.Screen name="index" />
+      <Stack.Screen name="account" />
     </Stack>
   );
 }
